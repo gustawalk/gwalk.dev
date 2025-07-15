45915da038315bc9436020f8f041dab2152dcf74
gustawalk <gustawalk@gmail.com>
2025-07-15 14:01:47 -0300

    alguns itens a menos

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import path from 'path';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import path from "path";
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@views": path.resolve(__dirname, "./src/views"),
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@views': path.resolve(__dirname, './src/views'),
     },
   },
   server: {
+    host: '0.0.0.0',
     port: 3011,
-    host: true,
-    strictPort: true,
-    allowedHosts: true,
   },
 })
